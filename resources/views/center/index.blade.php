8dbf909eb42788443476bcfb8a5e4ab8b3ec89b6
Kui_Ye <938424168@qq.com>
2015-07-14 18:15:36 +0800

    JS/CSS

---
+++ resources/views/center/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sunqichao
+ * Date: 15/7/14
+ * Time: 下午6:09
+ */
---
+++ resources/views/header.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <!--[if gt IE 8]>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <![endif]-->
+    <title>用户登录</title>
+    <link rel="icon" type="{{asset('image/ico')}}" href="favicon.ico"/>
+
+    <link href="{{asset('css/stylesheets.css')}}" rel="stylesheet" type="text/css" />
+    <!--[if lt IE 10]>
+    <link href="{{asset('css/ie.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css" />
+    <![endif]-->
+    <!--[if lte IE 7]>
+    <script type='text/javascript' src="{{asset('js/plugins/other/lte-ie7.js')}}"></script>
+    <![endif]-->
+    <script type='text/javascript' src="{{asset('js/plugins/jquery/jquery-1.9.1.min.js')}}"></script>
+    <script type='text/javascript' src="{{asset('js/plugins/jquery/jquery-ui-1.10.1.custom.min.js')}}"></script>
+    <script type='text/javascript' src="{{asset('js/plugins/jquery/jquery-migrate-1.1.1.min.js')}}"></script>
+    <script type='text/javascript' src="{{asset('js/plugins/jquery/globalize.js')}}"></script>
+    <script type='text/javascript' src="{{asset('js/plugins/other/excanvas.js')}}"></script>
+
+    <script type='text/javascript' src="{{asset('js/plugins/other/jquery.mousewheel.min.js')}}"></script>
+
+    <script type='text/javascript' src="{{asset('js/plugins/bootstrap/bootstrap.min.js')}}"></script>
+
+    <script type='text/javascript' src="{{asset('js/plugins/uniform/jquery.uniform.min.js')}}"></script>
+
+    <script type='text/javascript' src="{{asset('/js/plugins/shbrush/XRegExp.js')}}"></script>
+    <script type='text/javascript' src="{{asset('js/plugins/shbrush/shCore.js')}}"></script>
+    <script type='text/javascript' src="{{asset('js/plugins/shbrush/shBrushXml.js')}}"></script>
+    <script type='text/javascript' src="{{asset('js/plugins/shbrush/shBrushJScript.js')}}"></script>
+    <script type='text/javascript' src="{{asset('js/plugins/shbrush/shBrushCss.js')}}"></script>
+
+    <script type='text/javascript' src="{{asset('js/plugins.js')}}"></script>
+    <script type='text/javascript' src="{{asset('js/charts.js')}}"></script>
+    <script type='text/javascript' src="{{asset('js/actions.js')}}"></script>
+
+</head>
+	@yield('content')
+
+	<!-- Scripts -->
+
